d042b7bfedea055244a4cc963ca5b1727c3a4516
GASLTDA <36173692+GASLTDA@users.noreply.github.com>
2018-05-31 07:31:32 -0600

    crc_tipode_cambio/afterDBrestore

--- crc_tipode_cambio/__manifest__.py
+++ crc_tipode_cambio/__manifest__.py
@@ -14,7 +14,7 @@
     """,
     'website': "web-builds.com",
     'category': 'Currency',
-    'version': '1.0',
+    'version': '1.1',
     'depends': ['base','account'],
     'data': [
         'views/crc_currency_rate_view.xml',
